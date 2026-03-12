af230d87913fafc7b236f910bae789520a742348
fvcanquil <126803272+fvcanquil@users.noreply.github.com>
2026-03-12 18:45:45 -0300

    first commit

---
+++ src/main/java/com/duoc/veterinaria/GestionVeterinariaApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVeterinariaApplication.class, args);
+	}
+
+}
